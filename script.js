033f510a859e9394ec5c1e4e8078488809863b0f
Hasnain Khan🏴 <official.thevm@gmail.com>
2025-05-07 19:25:14 +0500

    done

---
+++ script.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+import { vertexShader, fragmentShader } from "./shaders";
+import { slides } from "./slides";
+
+const container = document.querySelector(".container");
+const projectTitle = document.getElementById("project-title");
+const projectLink = document.getElementById("project-link");
+
+let scrollIntensity = 0;
+let targetScrollIntensity = 0;
+const maxScrollIntensity = 1.0;
+const scrollSmoothness = 0.5;
+
+let scrollPosition = 0;
+let targetScrollPosition = 0;
+const scrollPositionSmoothness = 0.05;
+
+let isMoving = false;
+const movementThreshold = 0.001;
+let isSnapping = false;
+
+let stableCurrentIndex = 0;
+let stableNextIndex = 1;
+let isStable = false;
+
+let titleHidden = false;
+let titleAnimating = false;
+let currentProjectIndex = 0;
+
+projectTitle.textContent = slides[0].title;
+projectLink.href = slides[0].url;
---
+++ slides.js
@@ -0,0 +1,38 @@
+export const slides = [
+  {
+    title: "Chromatic Loopscape",
+    url: "https://example.com/alpha",
+    image:
+      "https://media.istockphoto.com/id/1714905230/photo/exercise-running-and-motion-blur-with-a-sports-man-on-a-road-for-his-cardio-or-endurance.jpg?s=612x612&w=0&k=20&c=Mc-AX_li-VnbDELsBU8bajU0ThRDX0SJQvwh3IHUgcw=",
+  },
+  {
+    title: "Solar Bloom",
+    url: "https://example.com/beta",
+    image: "/img2.jpg",
+  },
+  {
+    title: "Neon Handscape",
+    url: "https://example.com/gamma",
+    image: "/img3.jpg",
+  },
+  {
+    title: "Echo Discs",
+    url: "https://example.com/delta",
+    image: "/img4.jpg",
+  },
+  {
+    title: "Void Gaze",
+    url: "https://example.com/epsilon",
+    image: "/img5.jpg",
+  },
+  {
+    title: "Gravity Sync",
+    url: "https://example.com/epsilon",
+    image: "/img6.jpg",
+  },
+  {
+    title: "Heat Core",
+    url: "https://example.com/epsilon",
+    image: "/img7.jpg",
+  },
+];
